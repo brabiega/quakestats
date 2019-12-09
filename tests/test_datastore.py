ae1f18fc4ec8e34ad8dabd2a760cc3373b492779
BR <b.rabiega@gmail.com>
2019-12-09 21:30:13 +0100

    Dev: Fixed tests

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -56,6 +56,7 @@ class TestDataStoreMongo():
             {'player_id': 'p1', 'damage_dealt': 123},
             {'player_id': 'p2', 'damage_dealt': 125},
         ]
+        report.summary = 'dummy_summary'
         return report
 
     @pytest.fixture
@@ -94,6 +95,7 @@ class TestDataStoreMongo():
             'start_date': 1,
             'time_limit': 100,
             'score_limit': 1000,
+            'summary': 'dummy_summary',
         })
 
     def test_store_team_lifecycle(self, ds, report, stored_switches):
