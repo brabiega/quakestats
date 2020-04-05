19da02c23f647c6824c1a66a22887bb428ae0318
BR <b.rabiega@gmail.com>
2020-04-06 00:46:18 +0200

    Added better weapon stats panel

---
+++ quakestats/web/static/js/views/match.js
@@ -0,0 +1,66 @@
+class MatchView {
+    constructor(app) {
+        this.app = app
+        this.gui = {}
+        this.players = {}
+    }
+
+    run() {
+        this.gui.weaponStats = riot.mount('weapon-stats', { loading: true })[0]
+
+        Promise.all([this.fetch_players])
+            .then(resolution => {
+                var players = resolution[0]
+                for (let player of players) {
+                    this.players[player.id] = player
+                }
+            })
+
+        // fixme, howto combine promises with view object
+        Promise.all([this.fetch_match_player_stats, this.fetch_match_kills, this.fetch_players])
+            .then(resolution => {
+                var r_stats = resolution[0]
+                var r_kills = resolution[1]
+                this.setWeaponStats(r_stats, r_kills)
+            })
+    }
+
+    // r_ - raw_
+    setWeaponStats(r_stats, r_kills) {
+        // need to calculate all weapons (from kills + from weapon stats)
+        var finalStats = {}
+
+        // calculate deaths and kills per player per weapon
+        _.forEach(r_kills, (kill) => {
+            var weaponName = _.get(this.app.resources.weaponMap, kill.by, kill.by)
+                // ignore self kills, process world kills
+            if (kill.killer_id != kill.victim_id) {
+                // sum kills
+                var path = `${kill.killer_id}.stats.${weaponName}.kills`
+                _.set(finalStats, path, _.get(finalStats, path, 0) + 1)
+
+                // sum deaths
+                path = `${kill.victim_id}.stats.${weaponName}.deaths`
+                _.set(finalStats, path, _.get(finalStats, path, 0) + 1)
+            }
+        })
+
+        _.forEach(finalStats, (value, player_id) => {
+            // add accuracy info from weapon stats
+            var weaponStat = _.find(r_stats, { 'player_id': player_id })
+            _.forEach(value.stats, (ws, wname) => {
+                ws.hits = _.get(weaponStat, `weapons.${wname}.H`, null)
+                ws.shots = _.get(weaponStat, `weapons.${wname}.S`, null)
+            })
+            value.player_id = player_id
+            value.player = this.players[player_id]
+        })
+        this.gui.weaponStats.update({
+            opts: {
+                loading: false,
+                players: this.players,
+                stats: _.values(finalStats)
+            }
+        })
+    }
+}
